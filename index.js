f4aa7fce7c6b39b5b66a56e4c04f55cbd1e2797d
yuronghui <yuronghui@singworld.net>
2018-12-17 15:33:14 +0800

    no message

--- index.js
+++ index.js
@@ -5,17 +5,18 @@
  */
 const { Token } = require('wood-token')();
 
-module.exports = (app = {}, config = {}) => {
+module.exports = (app = {}, config = {idName: 'account_id'}) => {
   let { catchErr, error } = app;
   let RedisPlugin = app.Plugin('redis');
   if(RedisPlugin){
-    let Redis = new RedisPlugin.Redis('tokens', config.redis || 'master', app);
+    let Redis = new RedisPlugin.Redis(config.tableName || 'session', config.redis || 'master', app);
     app.Passport = async function(req, res, next){
       let theToken = req.headers.token;
       if(theToken){
         let userData = new Token().checkToken(theToken);
         if(userData){
-          let cacheToken = await Redis.getValue(userData.uid);
+          let key = userData.product_id ? `${userData.account_id}:${userData.product_id}` : userData.account_id;
+          let cacheToken = await Redis.getHaseValue(key);
           if(theToken === cacheToken){
             req.User = userData;
             next();
